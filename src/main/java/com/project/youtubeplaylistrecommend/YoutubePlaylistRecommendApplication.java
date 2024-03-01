621325d96d3131943ac7caac61cec59bc3349e00
이주영 <wruomma2@naver.com>
2024-03-01 15:03:26 +0900

    :tada: tada: 프로젝트 생성 및 의존성 주입

---
+++ src/main/java/com/project/youtubeplaylistrecommend/YoutubePlaylistRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.project.youtubeplaylistrecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubePlaylistRecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubePlaylistRecommendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/youtubeplaylistrecommend/YoutubePlaylistRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.youtubeplaylistrecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubePlaylistRecommendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
