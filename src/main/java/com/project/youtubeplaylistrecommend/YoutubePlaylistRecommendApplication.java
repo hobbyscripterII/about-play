d7f1965e12b72424548d4c6929e58978886749fe
이주영 <wruomma2@naver.com>
2024-03-06 01:17:56 +0900

    :memo: memo: jpa 설정 추가

--- src/main/java/com/project/youtubeplaylistrecommend/YoutubePlaylistRecommendApplication.java
+++ src/main/java/com/project/youtubeplaylistrecommend/YoutubePlaylistRecommendApplication.java
@@ -1,12 +1,24 @@
 package com.project.youtubeplaylistrecommend;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class YoutubePlaylistRecommendApplication {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(YoutubePlaylistRecommendApplication.class, args);
     }
